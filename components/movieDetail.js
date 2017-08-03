358d282b914136668900409ed2933a8fee9a44d7
Tom Jenkins <tjenkins@remedyhealthmedia.com>
2017-08-03 10:40:49 -0400

    update movieDetail component to display more info

--- components/movieDetail.js
+++ components/movieDetail.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 function MovieDetail({ movie }) {
+  const imdb = `http://www.imdb.com/title/${movie.imdb_id}/`;
   return (
-    <h2>
-      {movie.title}
-    </h2>
+    <div>
+      <h2>
+        <a href={movie.homepage}>{movie.title}</a>
+      </h2>
+      <p>{movie.overview}</p>
+      <p>imdb: <a href={imdb}>{imdb}</a></p>
+    </div>
   );
 }
+
+MovieDetail.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    imdb_id: PropTypes.string,
+    homepage: PropTypes.string,
+    overview: PropTypes.string,
+  }),
+};
+
+MovieDetail.defaultProps = {
+  movie: {},
+};
 
 function mapStateToProps(state, ownProps) {
   const { id } = ownProps;
